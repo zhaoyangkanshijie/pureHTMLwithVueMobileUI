321517df1f5b6e223ee178d8977ee69899dfd5a1
zhaoyangkanshijie <2191732408@qq.com>
2020-07-17 18:04:46 +0800

    mint ui

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data() {
         return {
-            visible: false
-        }
+            visible: false,
+        };
     },
     methods: {
-        
+        open() {
+            this.$alert("这是一段内容", "标题名称", {
+                confirmButtonText: "确定",
+                callback: (action) => {
+                    this.$message({
+                        type: "info",
+                        message: `action: ${action}`,
+                    });
+                },
+            });
+        },
     },
-    computed: {
-        
-    },
-    watch: {
-        
-    },
-    mounted() {
-
-    },
-    beforeDestroy() {
-        
-    }
-})
+    computed: {},
+    watch: {},
+    mounted() { },
+    beforeDestroy() { },
+});
