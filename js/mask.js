c862a8ee7b67f87f5d8844b7531ce43caf898ba5
zhaoyangkanshijie <2191732408@qq.com>
2021-05-07 15:02:36 +0800

    component

---
+++ js/mask.js
@@ -0,0 +1,18 @@
+﻿let myMaskTemplate = `
+<div>
+    <div>{{ todo.text }}</div>
+    <slot></slot>
+</div>
+`;
+Vue.component('my-mask', {
+    template: myMaskTemplate,
+    props: {
+        todo: [String]
+    },
+    data() {
+
+    },
+    mounted() {
+        this.$emit('emit')
+    }
+});
